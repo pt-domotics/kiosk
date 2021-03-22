c56190c8488a1416ea6bd975f9317afa550d95e2
Nuno Santos <eu.nuno@gmail.com>
2021-03-22 13:06:51 +0000

    Initial development

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+const width = parseInt(process.env.KIOSK_WIDTH) || 800;
+const height = parseInt(process.env.KIOSK_HEIGHT) || 480;
+const backgroundColor = process.env.KIOSK_BG || '#000000';
+const frame = (parseInt(process.env.KIOSK_FRAME) || 0) !== 0;
+const resizable =  (parseInt(process.env.KIOSK_RESIZABLE) || 0) !== 0;
+const alwaysOnTop = (parseInt(process.env.KIOSK_ONTOP) || 1) !== 0;
+const fullscreen = (parseInt(process.env.KIOSK_FULLSCREEN) || 0) !== 0;
+const devTools = (parseInt(process.env.KIOSK_DEVTOOLS) || 0) !== 0;
+const useCache = (parseInt(process.env.KIOSK_USECACHE) || 0) !== 0;
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width,
+    height,
+    useContentSize: true,
+    backgroundColor,
+    frame,
+    resizable,
+    alwaysOnTop,
+    fullscreen,
+    webPreferences: {
+      devTools,
+      contextIsolation: true
+    }
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadURL(process.env.KIOSK_URL || 'about:blank');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if(devTools) {
+    mainWindow.webContents.openDevTools({
+        mode: 'detach'
+    });
+  }
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+if(!useCache) {
+  app.commandLine.appendSwitch("disable-http-cache");
+}
+
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -35,12 +48,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
